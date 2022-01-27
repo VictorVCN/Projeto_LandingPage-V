c78ccf93c99b880e133f535d40cc6ed56e3e669d
Victor Costa Nascimento <98558465+VictorVCN@users.noreply.github.com>
2022-01-27 19:27:10 -0300

    Add files via upload

---
+++ src/js/painel.js
@@ -0,0 +1,57 @@
+/* "const" é usado para manter a variável inalterada */ 
+
+const imagensPainel = document.querySelectorAll('.imagem-painel');
+const setaAvancar = document.getElementById('btn-avancar');
+const setaVoltar = document.getElementById('btn-voltar');
+let imagemAtual = 0;
+
+function esconderImagens() {
+    imagensPainel.forEach(imagem => {
+        imagem.classList.remove('mostrar')
+    });
+}
+
+function mostrarImagens() {
+    imagensPainel[imagemAtual].classList.add('mostrar');
+}
+
+setaAvancar.addEventListener('click',function() {
+
+    // Testa se o contador da imagemAtual é igual ao total de imagens
+    const totalDeImagens = imagensPainel.length - 1;
+    if(imagemAtual === totalDeImagens) {
+        return;
+    }
+
+    imagemAtual++;
+
+    esconderImagens();
+    mostrarImagens();
+});
+
+setaVoltar.addEventListener('click', function() {
+
+    if(imagemAtual === 0) {
+        return;
+    }
+
+    imagemAtual--;
+
+    esconderImagens();
+    mostrarImagens();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
